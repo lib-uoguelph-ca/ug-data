34408504b93a9c55d48bbad3ef525fb2e0c4191b
Adam Doan <doana@uoguelph.ca>
2013-02-07 10:45:50 -0500

    Now with vagrant configuration.

---
+++ puppet/modules/xdebug/manifests/debian.pp
@@ -0,0 +1,30 @@
+class xdebug::debian {
+
+    include xdebug::params
+    
+    package { "xdebug":
+        name   => $xdebug::params::pkg,
+        ensure => installed,
+        require => Class['lamp'],
+    }
+ 
+    file { '/etc/php5/conf.d/xdebug.ini':
+	ensure => file,
+	owner  => '0',
+	group  => '0',
+	mode   => '0644',
+	content => 
+	'
+   xdebug.default_enable=1
+	xdebug.remote_enable=1
+	xdebug.remote_handler=dbgp
+	xdebug.remote_host=33.33.33.1
+	xdebug.remote_port=9000
+	xdebug.remote_autostart=1
+	',
+    }
+
+    exec {'restart apache':
+	command => '/etc/init.d/apache2 restart',
+    }
+}
